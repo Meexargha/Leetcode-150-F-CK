98be19b6ef9040a391678193e94ce36f0742be01
Argha kundu <128323651+Meexargha@users.noreply.github.com>
2025-09-23 19:35:13 +0530

    Create 121. Best Time to Buy and Sell Stock.cpp

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int i = 1;
+        int max_profit= 0;
+        int n = prices.size();
+        while(i<n){
+
+           if (prices[i] < min_price){
+
+           min_price = prices[i];
+           }else{
+            max_profit=max(max_profit, prices[i]-min_price);
+           }
+           i++;
+            
+        }
+        
+        return max_profit;
+    }
+};
