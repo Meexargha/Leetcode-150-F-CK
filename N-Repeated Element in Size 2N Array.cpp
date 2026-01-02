47e56a8c2ba6f8c0ce5a44497c1ca39fa1f9b849
Argha kundu <128323651+Meexargha@users.noreply.github.com>
2026-01-02 21:54:40 +0530

    Implement solution for N-Repeated Element problem

---
+++ N-Repeated Element in Size 2N Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int>seen;
+        for(int i=0; i<nums.size(); i++){
+            int x=nums[i];
+
+            if(seen.count(x)){
+                return x;
+            }
+            seen.insert(x);
+        }
+        return -1;
+    }
+};
